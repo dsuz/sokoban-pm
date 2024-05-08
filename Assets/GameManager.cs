1032dfb1b6b455cbdfe5c57782d67e570cba05f0
Daisuke Suzuki <bboydaisuke@gmail.com>
2024-05-08 23:41:20 +0900

    5/25の授業分を更新した（「第01回 01_移動(配列操作)」の最後まで）

---
+++ Assets/GameManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int[] map;
+
+    void Start()
+    {
+        map = new int[] { 0, 0, 0, 1, 0, 0, 0, 0, 0 };
+        string debugText = "";
+
+        for (int i = 0; i < map.Length; i++)
+        {
+            debugText += map[i].ToString() + ","; 
+        }
+
+        Debug.Log(debugText);
+    }
+
+    void Update()
+    {
+        bool debugOut = false;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            int playerIndex = -1;
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                if (map[i] == 1)
+                {
+                    playerIndex = i;
+                    break;
+                }
+            }
+
+            if (playerIndex < map.Length - 1)
+            {
+                map[playerIndex + 1] = 1;
+                map[playerIndex] = 0;
+            }
+
+            debugOut = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            int playerIndex = -1;
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                if (map[i] == 1)
+                {
+                    playerIndex = i;
+                    break;
+                }
+            }
+
+            if (playerIndex > 0)
+            {
+                map[playerIndex - 1] = 1;
+                map[playerIndex] = 0;
+            }
+
+            debugOut = true;
+        }
+
+        if (debugOut)
+        { 
+            string debugText = "";
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                debugText += map[i].ToString() + ",";
+            }
+
+            Debug.Log(debugText);
+        }
+    }
+}
